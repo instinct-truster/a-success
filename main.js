8aebe5cdc3364de0a1efd161a24883333299d8d5
Lance Turbes <instinct_truster@protonmail.com>
2023-09-18 14:45:59 -0700

    feat: menu toggle animation

--- main.js
+++ main.js
@@ -10,3 +10,9 @@
     });
   });
 })();
+
+let toggle = document.querySelector(".toggle");
+let menu = document.querySelector(".menu");
+toggle.onclick = function () {
+  menu.classList.toggle("active");
+};
