782382f155a06096b1fa312420b885886b71d41d
Lance Turbes <instinct_truster@protonmail.com>
2024-09-16 14:06:18 -0700

    separated out

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-(function setGlowEffectRx() {
-  const glowEffects = document.querySelectorAll(".glow-effect");
-
-  glowEffects.forEach((glowEffect) => {
-    const glowLines = glowEffect.querySelectorAll("rect");
-    const rx = getComputedStyle(glowEffect).borderRadius;
-
-    glowLines.forEach((line) => {
-      line.setAttribute("rx", rx);
-    });
-  });
-})();
-
-let toggle = document.querySelector(".toggle");
-let menu = document.querySelector(".menu");
+let toggle = document.querySelector('.radial-menu-toggle');
+let menu = document.querySelector('.radial-menu');
 toggle.onclick = function () {
-  menu.classList.toggle("active");
+  menu.classList.toggle('active');
 };
